d924ab2cd74b46e5309f2f15db2641bf0563d71d
Haowen Liu <haowen.liu@shopee.com>
2021-03-26 10:52:22 +0800

    加载资源变化清单

--- webpack-dev-server/client/dev-server.js
+++ webpack-dev-server/client/dev-server.js
@@ -1,6 +1,35 @@
 console.log('client: dev-server.js')
 const hotEmitter = require('./emitter')
 
+let currentHash
+let lastHash
+function hotCheck() {
+  hotDownloadManifest().then(res => {
+    console.log('hot-update.json ->', res)
+  })
+}
+
+function hotDownloadManifest() {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest()
+    xhr.open('get', `main.${lastHash}.hot-update.json`)
+    xhr.onload = function() {
+      resolve(JSON.parse(xhr.responseText))
+    }
+    xhr.onerror = function(e) {
+      reject(e)
+    }
+    xhr.send()
+  })
+}
+
 hotEmitter.on('webpackHotUpdate', (hash) => {
   console.log('in client dev-server hash ->', hash)
+  if (!lastHash) {
+    lastHash = hash
+    return
+  }
+
+  currentHash = hash
+  hotCheck()
 })
